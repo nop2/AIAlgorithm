d0bfa11673dc3aee84f058ac8ab68e58ed5cd108
ErrorXin <42483111+2845963383@users.noreply.github.com>
2020-11-16 18:40:36 +0800

    添加项目文件。

---
+++ 2/八数码.cpp
@@ -0,0 +1,319 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <vector>
+#include <string>
+#include <map>
+#include <stack>
+#include <ctime>
+#include <queue>
+#include <memory>
+using namespace std;
+
+//节点，保存棋盘信息
+class Node
+{
+public:
+	static vector<vector<int>> target; //目标状态
+	vector<vector<int>> board; //棋盘状态
+	map<int, int> posMap; //棋盘数字位置映射
+	shared_ptr<Node> parent; //父节点,用于构造路径
+	int size; //棋盘长度
+	int step = 0; //当前移动次数
+	int op = 0; //上一步操作
+
+	Node(vector<vector<int>> board, int step = 0, int op = 0, shared_ptr<Node> parent = nullptr, int mode = 1)
+	{
+		this->board = board;
+		this->step = step;
+		this->op = op;
+		this->mode = mode;
+		this->size = board.size();
+		this->parent = parent;
+
+		int counter = 0;
+		for (int i = 0; i < size; ++i)
+		{
+			for (int j = 0; j < size; ++j)
+			{
+				posMap[board[i][j]] = counter++;
+			}
+		}
+	}
+
+	string ToString()
+	{
+		string s;
+		for (auto& row : board)
+		{
+			for (int num : row)
+			{
+				s.push_back('a' + num);
+			}
+		}
+		return s;
+	}
+
+	void Print()
+	{
+		for (auto& row : board)
+		{
+			for (int num : row)
+			{
+				cout << num << " ";
+			}
+			cout << endl;
+		}
+	}
+
+	bool isTarget()
+	{
+		for (int i = 0; i < size; ++i)
+		{
+			for (int j = 0; j < size; ++j)
+			{
+				if (board[i][j] != target[i][j])
+				{
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	//启发信息函数,值越小越好
+	int f()
+	{
+		if (mode == 0)
+		{
+			//return 0;
+			return g();
+		}
+		if (mode == 1)
+		{
+			return g() + h1();
+		}
+		return g() + h2();
+	}
+
+private:
+	//启发信息
+	int mode; //估价函数选择
+
+	//耗费函数
+	int g()
+	{
+		return step;
+	}
+
+	//估价函数1, 错位的个数
+	int h1()
+	{
+		int sum = 0;
+
+		for (int i = 0; i < size; ++i)
+		{
+			for (int j = 0; j < size; ++j)
+			{
+				if (board[i][j] != target[i][j])
+				{
+					++sum;
+				}
+			}
+		}
+		return sum;
+	}
+
+	//估价函数2，错误位置到正确位置的距离和
+	int h2()
+	{
+		int sum = 0;
+		for (int i = 0; i < size; ++i)
+		{
+			for (int j = 0; j < size; ++j)
+			{
+				int pos = posMap[board[i][j]];
+				int x = pos / size;
+				int y = pos % size;
+				sum += abs(x - i) + abs(y - j);
+			}
+		}
+		return sum;
+	}
+};
+
+//棋盘类，封装了节点
+class ChessBoard
+{
+public:
+	shared_ptr<Node> pNode; //节点指针
+	ChessBoard(shared_ptr<Node> pNode)
+	{
+		this->pNode = pNode;
+	}
+
+	bool operator<(const ChessBoard chessBoard) const
+	{
+		return pNode->f() > chessBoard.pNode->f();
+	}
+};
+
+//估价函数
+enum MODE
+{
+	BFS = 0,
+	WRONG_POSITION = 1,
+	DISTANCE_BETWEEN_WRONG_POSITION = 2
+};
+
+//生成扩展节点的所有子节点并加入open表
+void GenerateNode(priority_queue<ChessBoard>& queue, ChessBoard& frontChessBoard, int x, int y, int nx, int ny, int op,
+                  int mode);
+//搜索解
+void Solve(vector<vector<int>> start, vector<vector<int>> target, MODE mode);
+
+
+vector<vector<int>> Node::target = vector<vector<int>>();
+map<string, bool> boardMap; //记录已出现过的棋盘
+string rows[4] = {"↑", "↓", "←", "→"};
+int expandingNodeCounter = 0; //扩展节点数
+int generatingNodeCounter = 0; //生成节点数
+
+
+int main()
+{
+	//初始节点状态
+	auto start2 = vector<vector<int>>{
+		{4, 5, 6},
+		{0, 1, 2},
+		{3, 7, 8}
+	};
+	auto start1 = vector<vector<int>>{
+		{1, 3, 4},
+		{8, 0, 6},
+		{2, 7, 5}
+	};
+	//目标状态
+	auto targetBoard = vector<vector<int>>{
+		{1, 2, 3},
+		{8, 0, 4},
+		{7, 6, 5}
+	};
+
+	//搜索解
+	Solve(start2,targetBoard,MODE::WRONG_POSITION);
+	
+}
+
+void Solve(vector<vector<int>> start, vector<vector<int>> target, MODE mode)
+{
+	//int mode = MODE::WRONG_POSITION; //估价函数
+	
+	//初始化起始节点
+	shared_ptr<Node> startNode(new Node(std::move(start)));
+	startNode->target = std::move(target);
+	boardMap[(*startNode).ToString()] = true;
+
+	//节点open表，使用优先队列实现
+	priority_queue<ChessBoard> openedList;
+
+	//记录启动时间
+	auto startClock = clock();
+
+	openedList.push(ChessBoard(startNode));
+	//扩展节点直到找到解或open表为空
+	while (!openedList.empty())
+	{
+		auto frontChessBoard = openedList.top();
+		openedList.pop();
+		//判断是否搜索到目标状态
+		if (frontChessBoard.pNode->isTarget())
+		{
+			//搜索完成时间
+			auto clockTime = clock() - startClock;
+			cout << "Solved in " << static_cast<double>(clockTime) / CLOCKS_PER_SEC << "s." << endl;
+			cout << "Total step：" << frontChessBoard.pNode->step << endl;
+			cout << "Number of extension nodes: " << expandingNodeCounter << endl;
+			cout << "Number of generated nodes: " << generatingNodeCounter << endl;
+			cout << endl;
+			//将节点压栈倒推路径
+			stack<shared_ptr<Node>> nodeStack;
+			do
+			{
+				nodeStack.push(frontChessBoard.pNode);
+				frontChessBoard = frontChessBoard.pNode->parent;
+			} while (frontChessBoard.pNode != nullptr);
+
+			while (!nodeStack.empty())
+			{
+				auto node = nodeStack.top();
+				nodeStack.pop();
+				if (node->step > 0)
+				{
+					cout << "Step " << node->step << ". " << rows[node->op] << endl;
+				}
+				node->Print();
+				cout << endl;
+			}
+
+			return;
+		}
+
+		//扩展节点数+1
+		++expandingNodeCounter;
+		//计算0所在位置
+		int pos0 = frontChessBoard.pNode->posMap[0];
+		int x = pos0 / frontChessBoard.pNode->size;
+		int y = pos0 % frontChessBoard.pNode->size;
+
+		//扩展所可能节点
+		if (x - 1 >= 0)
+		{
+			GenerateNode(openedList, frontChessBoard, x, y, x - 1, y, 0, mode);
+		}
+		if (x + 1 < frontChessBoard.pNode->size)
+		{
+			GenerateNode(openedList, frontChessBoard, x, y, x + 1, y, 1, mode);
+		}
+		if (y - 1 >= 0)
+		{
+			GenerateNode(openedList, frontChessBoard, x, y, x, y - 1, 2, mode);
+		}
+		if (y + 1 < frontChessBoard.pNode->size)
+		{
+			GenerateNode(openedList, frontChessBoard, x, y, x, y + 1, 3, mode);
+		}
+	}
+
+	cout << "未找到解" << endl;
+}
+
+
+void GenerateNode(priority_queue<ChessBoard>& queue, ChessBoard& frontChessBoard, int x, int y, int nx, int ny, int op,
+                  int mode)
+{
+	auto vectorTmp = frontChessBoard.pNode->board;
+	swap(vectorTmp[nx][ny], vectorTmp[x][y]);
+	string s;
+	for (auto& row : vectorTmp)
+	{
+		for (int num : row)
+		{
+			s.push_back('a' + num);
+		}
+	}
+	//将符合条件的子节点加入open表
+	if (boardMap[s] == false)
+	{
+		boardMap[s] = true;
+		shared_ptr<Node> newNode(new Node(vectorTmp, frontChessBoard.pNode->step + 1, op, frontChessBoard.pNode, mode));
+		queue.push(ChessBoard(newNode));
+		//生成节点数+1
+		++generatingNodeCounter;
+	}
+}
+
+/*
+ *不在位距离和的计算消耗资源较多或该估价函数根本不合适，性能要差于BFS
+ *
+ */
---
+++ 井字棋alpha-beta/Program.cs
@@ -0,0 +1,466 @@
+﻿using System;
+
+namespace 井字棋alpha_beta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var board = new ChessBoard(new[,] {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}});
+            // board.Put(1, 2, 1);
+            // board.UnPut(1, 2);
+            // Console.WriteLine(board.Size);
+            // Console.WriteLine(board.IsEmpty());
+            // Console.WriteLine(board.IsFull());
+
+            var game = new Game();
+            game.Start();
+        }
+    }
+
+    class ChessBoard
+    {
+        //棋盘状态
+        public int[,] Board { get; private set; }
+
+        //空闲状态标志
+        public int EmptyFlag { get; private set; }
+
+        //棋子数量
+        public int Size { get; private set; } = 0;
+
+        public ChessBoard(int[,] board, int emptyFlag = 0)
+        {
+            Board = board;
+            EmptyFlag = emptyFlag;
+        }
+
+        //检测棋盘是否为空或指定位置是否为空
+        public bool IsEmpty(int x = -1, int y = -1)
+        {
+            if (x == -1 && y == -1)
+            {
+                return Size == 0;
+            }
+
+            return Board[x, y] == 0;
+        }
+
+        //检测棋盘是否放满
+        public bool IsFull() => Size == Board.GetLength(0) * Board.GetLength(1);
+
+        //放置棋子
+        public void Put(int x, int y, int flag)
+        {
+            Board[x, y] = flag;
+            ++Size;
+        }
+
+        //取消放置棋子
+        public void UnPut(int x, int y)
+        {
+            Board[x, y] = EmptyFlag;
+            --Size;
+        }
+    }
+
+    class Game
+    {
+        //棋盘
+        private ChessBoard chessBoard;
+
+        private const int Inf = 20;
+
+        public Game()
+        {
+            chessBoard = new ChessBoard(new[,] {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}});
+        }
+
+        //开始游戏
+        public void Start()
+        {
+            Console.WriteLine("是否先下棋？");
+            var command = int.Parse(Console.ReadLine());
+            if (command == 1)
+            {
+                Console.WriteLine("请输入：");
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                HumanPut(a, b);
+            }
+
+            while (true)
+            {
+                FindComputerMove(3, -Inf, Inf, out int x, out int y);
+                Console.WriteLine($"Computer：{x},{y}");
+                chessBoard.Put(x, y, (int) Player.Computer);
+                if (IsWin(Player.Computer))
+                {
+                    Console.WriteLine("电脑获胜！");
+                    break;
+                }
+
+                if (chessBoard.IsFull())
+                {
+                    Console.WriteLine("平手！");
+                    break;
+                }
+
+                Console.WriteLine("请输入：");
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                HumanPut(a, b);
+                if (IsWin(Player.Human))
+                {
+                    Console.WriteLine("你获胜！！");
+                    break;
+                }
+
+                if (chessBoard.IsFull())
+                {
+                    Console.WriteLine("平手！");
+                    break;
+                }
+            }
+        }
+
+        //计算当前局面评估值，站在电脑的角度
+        public int Evaluate()
+        {
+            int humanScore = 0, computerScore = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                bool humanExistRow = false, computerExistRow = false;
+                bool humanExistCol = false, computerExistCol = false;
+
+                for (int j = 0; j < 3; j++)
+                {
+                    if (chessBoard.Board[i, j] == (int) Player.Human)
+                    {
+                        humanExistRow = true;
+                    }
+
+                    if (chessBoard.Board[i, j] == (int) Player.Computer)
+                    {
+                        computerExistRow = true;
+                    }
+
+                    if (chessBoard.Board[j, i] == (int) Player.Human)
+                    {
+                        humanExistCol = true;
+                    }
+
+                    if (chessBoard.Board[j, i] == (int) Player.Computer)
+                    {
+                        computerExistCol = true;
+                    }
+                }
+
+                if (!humanExistRow)
+                {
+                    ++computerScore;
+                }
+
+                if (!humanExistCol)
+                {
+                    ++computerScore;
+                }
+
+                if (!computerExistRow)
+                {
+                    ++humanScore;
+                }
+
+                if (!computerExistCol)
+                {
+                    ++humanScore;
+                }
+            }
+
+            bool humanExistDiagonal1 = false;
+            bool humanExistDiagonal2 = false;
+            bool computerExistDiagonal1 = false;
+            bool computerExistDiagonal2 = false;
+            if (chessBoard.Board[0, 0] == (int) Player.Human || chessBoard.Board[1, 1] == (int) Player.Human ||
+                chessBoard.Board[2, 2] == (int) Player.Human)
+            {
+                humanExistDiagonal1 = true;
+            }
+
+            if (chessBoard.Board[0, 0] == (int) Player.Computer || chessBoard.Board[1, 1] == (int) Player.Computer ||
+                chessBoard.Board[2, 2] == (int) Player.Computer)
+            {
+                computerExistDiagonal1 = true;
+            }
+
+            if (chessBoard.Board[2, 0] == (int) Player.Human || chessBoard.Board[1, 1] == (int) Player.Human ||
+                chessBoard.Board[0, 2] == (int) Player.Human)
+            {
+                humanExistDiagonal2 = true;
+            }
+
+            if (chessBoard.Board[2, 0] == (int) Player.Computer || chessBoard.Board[1, 1] == (int) Player.Computer ||
+                chessBoard.Board[0, 2] == (int) Player.Computer)
+            {
+                computerExistDiagonal2 = true;
+            }
+
+            if (!humanExistDiagonal1)
+            {
+                ++computerScore;
+            }
+
+            if (!humanExistDiagonal2)
+            {
+                ++computerScore;
+            }
+
+            if (!computerExistDiagonal1)
+            {
+                ++humanScore;
+            }
+
+            if (!computerExistDiagonal2)
+            {
+                ++humanScore;
+            }
+
+            //Console.WriteLine($"computer:{computerScore} human:{humanScore}");
+
+            return computerScore - humanScore;
+        }
+
+        public void HumanPut(int x, int y)
+        {
+            chessBoard.Put(x, y, (int) Player.Human);
+        }
+
+        //alpha-beta剪枝搜索
+        private int AlphaBetaSearch(int depth, int alpha, int beta, out int x, out int y)
+        {
+            x = -1;
+            y = -1;
+            if (chessBoard.IsFull())
+            {
+                return 0;
+            }
+
+            if (depth == 0)
+            {
+                return Evaluate();
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (!chessBoard.IsEmpty(i, j)) continue;
+                    chessBoard.Put(i, j, (int) Player.Computer);
+                    int value = -AlphaBetaSearch(depth - 1, -beta, -alpha, out int x1, out int y1);
+                    chessBoard.UnPut(i, j);
+                    if (value >= beta)
+                    {
+                        x = i;
+                        y = j;
+                        return beta;
+                    }
+
+                    if (value > alpha)
+                    {
+                        alpha = value;
+                    }
+                }
+            }
+
+            return alpha;
+        }
+
+        /// <summary>
+        /// 找到合适的放置棋子位置，使本层alpha值最大，站在电脑的角度
+        /// </summary>
+        /// <param name="maxDepth">搜素最大深度</param>
+        /// <param name="alpha"></param>
+        /// <param name="beta"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private int FindComputerMove(int maxDepth, int alpha, int beta, out int x, out int y)
+        {
+            x = 0;
+            y = 0;
+            // if (chessBoard.IsFull())
+            // {
+            //     return Inf;
+            // }
+
+            if (maxDepth == 0 || chessBoard.Size == 8)
+            {
+                return Evaluate();
+            }
+
+            if (IsBoundToWin(Player.Computer, out int x1, out int y1))
+            {
+                x = x1;
+                y = y1;
+                return Inf;
+            }
+
+            int value = alpha;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    //当前在MAX节点，进行beta剪枝，beta为上层MIN节点当前最小值
+                    if (value >= beta)
+                    {
+                        return value;
+                    }
+
+                    if (chessBoard.IsEmpty(i, j))
+                    {
+                        chessBoard.Put(i, j, (int) Player.Computer);
+                        int temp = FindHumanMove(maxDepth - 1, value, beta, out _, out _);
+                        chessBoard.UnPut(i, j);
+                        if (temp > value)
+                        {
+                            value = temp;
+                            x = i;
+                            y = j;
+                        }
+                    }
+                }
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// MIN节点,
+        /// </summary>
+        /// <param name="maxDepth"></param>
+        /// <param name="alpha"></param>
+        /// <param name="beta"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private int FindHumanMove(int maxDepth, int alpha, int beta, out int x, out int y)
+        {
+            x = 0;
+            y = 0;
+            // if (chessBoard.IsFull())
+            // {
+            //     return -Inf;
+            // }
+
+            if (maxDepth == 0 || chessBoard.Size == 8)
+            {
+                return Evaluate();
+            }
+
+            if (IsBoundToWin(Player.Human, out int x1, out int y1))
+            {
+                x = x1;
+                y = y1;
+                return -Inf;
+            }
+
+            int value = beta;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    //当前在MIN节点，进行alpha剪枝，用到alpha值
+                    if (value <= alpha)
+                    {
+                        return value;
+                    }
+
+                    if (chessBoard.IsEmpty(i, j))
+                    {
+                        chessBoard.Put(i, j, (int) Player.Human);
+                        int temp = FindComputerMove(maxDepth - 1, alpha, value, out _, out _);
+                        chessBoard.UnPut(i, j);
+                        if (temp < value)
+                        {
+                            value = temp;
+                            x = i;
+                            y = j;
+                        }
+                    }
+                }
+            }
+
+            return value;
+        }
+
+        public bool IsBoundToWin(Player player, out int x, out int y)
+        {
+            x = 0;
+            y = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (chessBoard.IsEmpty(i, j))
+                    {
+                        chessBoard.Put(i, j, (int) player);
+                        if (IsWin(player))
+                        {
+                            x = i;
+                            y = j;
+                            chessBoard.UnPut(i, j);
+                            return true;
+                        }
+
+                        chessBoard.UnPut(i, j);
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public bool IsWin(Player player)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                bool allRow = true;
+                bool allCol = true;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (chessBoard.Board[i, j] != (int) player)
+                    {
+                        allRow = false;
+                    }
+
+                    if (chessBoard.Board[j, i] != (int) player)
+                    {
+                        allCol = false;
+                    }
+                }
+
+                if (allCol || allRow)
+                {
+                    return true;
+                }
+            }
+
+            if ((chessBoard.Board[0, 0] == (int) player && chessBoard.Board[1, 1] == (int) player &&
+                 chessBoard.Board[2, 2] == (int) player)
+                || (chessBoard.Board[0, 2] == (int) player && chessBoard.Board[1, 1] == (int) player &&
+                    chessBoard.Board[2, 0] == (int) player))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+
+    enum Player
+    {
+        Computer = 1,
+        Human = 2
+    }
+}
